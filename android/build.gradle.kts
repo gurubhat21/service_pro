a4ad8cffb6eaaff55a5d27da802fe49896ac9282
gurubhat21 <gurubhat21@gmail.com>
2026-08-04 00:53:49 +0530

    Revert afterEvaluate kotlin apply, enable core library desugaring for flutter_local_notifications

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,17 +5,6 @@ allprojects {
     }
 }
 
-subprojects {
-    afterEvaluate {
-        if (project.plugins.hasPlugin("com.android.library") ||
-            project.plugins.hasPlugin("com.android.application")) {
-            if (!project.plugins.hasPlugin("org.jetbrains.kotlin.android")) {
-                project.apply(plugin = "org.jetbrains.kotlin.android")
-            }
-        }
-    }
-}
-
 val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
